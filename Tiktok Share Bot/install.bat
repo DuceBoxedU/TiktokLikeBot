613a214f236c8b885d3b0eb1fb22d7f23ecabc05
Duce Boxed U <106915154+DuceBoxedU@users.noreply.github.com>
2022-07-07 21:54:24 +0200

    Add files via upload

---
+++ Tiktok Share Bot/install.bat
@@ -0,0 +1,12 @@
+@echo off
+pip install colorama
+pip install tqdm
+pip install discord
+pip install pyinstaller
+pip install pyperclip
+pip install colour
+pip install PyNaCl
+pip install discord
+pip install psutil
+cls
+echo py Tiktok Like Bot.py
